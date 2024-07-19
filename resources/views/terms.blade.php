1331b1bada29479f35b260cd82590be43a66caf3
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-07-19 23:05:52 +0430

    Create the initial setup for the project

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('content')
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quidem ut id, necessitatibus, cupiditate accusamus
+    repudiandae voluptatibus commodi ipsum iste in atque, ad deserunt? Suscipit officia laudantium facilis officiis
+    dolores.</p>
+@endsection
