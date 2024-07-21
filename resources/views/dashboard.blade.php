13095b1db430e40183b3b27b4fede445c44c6615
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-07-21 22:30:11 +0430

    Add pagination to the application

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -46,6 +46,9 @@
           @include("partials._idea-card")
         </div>
       @endforeach
+      <div class="my-3">
+        {{ $ideas->links() }}
+      </div>
     </div>
     <div class="col-3">
       <div class="card">
