31f8fa1bc454424610343f98bbc1b04332e1b556
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-08 01:09:25 +0430

    Fix a typo error from the err_message partial

--- resources/views/partials/_error-message.blade.php
+++ resources/views/partials/_error-message.blade.php
@@ -1,6 +1,6 @@
-@if (session()->has("error"))
-  <div class="alert alert-daner alert-dismissible fade show" role="alert">
-    {{ session("error") }}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+@if (session()->has('error'))
+  <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    {{ session('error') }}
+    <button class="btn-close" data-bs-dismiss="alert" type="button" aria-label="Close"></button>
   </div>
 @endif
