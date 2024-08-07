96f292d2e886857d526f8faa0d0e28ffb7470e86
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-08 01:08:55 +0430

    Remove EnsureUserIsAdmin from the app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\EnsureUserIsAdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,9 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin' => EnsureUserIsAdminMiddleware::class
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
