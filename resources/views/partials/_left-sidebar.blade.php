e1c7715cc10deb76290eb28d93bbf20c5e288c87
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-07 22:22:32 +0430

    Add the feed item to the left side bar

--- resources/views/partials/_left-sidebar.blade.php
+++ resources/views/partials/_left-sidebar.blade.php
@@ -6,6 +6,10 @@
           <span>Home</span></a>
       </li>
       <li class="nav-item">
+        <a class="nav-link {{ Route::is('feed') ? 'text-bg-primary rounded' : '' }}" href="{{ route('feed') }}">
+          <span>Feed</span></a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link {{ Route::is('terms') ? 'text-bg-primary rounded' : '' }}" href="{{ route('terms') }}">
           <span>Terms</span></a>
       </li>
