e9a0ec41397eed7f4c5427389285f713e3006116
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-08 01:11:06 +0430

    Implement Gate for the admin panel in the routes.

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('home');
-Route::get('/admin', [AdminDashboardController::class, 'index'])->name('admin.dashboard')->middleware(['auth', 'admin']);
+Route::get('/admin', [AdminDashboardController::class, 'index'])->name('admin.dashboard')->middleware(['auth', 'can:admin']);
 Route::get('/feed', FeedController::class)->name('feed')->middleware('auth');
 
 Route::get('/terms', function () {
