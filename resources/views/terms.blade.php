1f4da1a5233ec8c077a491668b643c7d0156893e
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-07 17:47:35 +0430

    Customize the terms page to have sidebars

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.layout')
 
 @section('content')
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quidem ut id, necessitatibus, cupiditate accusamus
-    repudiandae voluptatibus commodi ipsum iste in atque, ad deserunt? Suscipit officia laudantium facilis officiis
-    dolores.</p>
+  <div class="row">
+    <div class="col-3">
+      @include('partials._left-sidebar')
+
+    </div>
+    <div class="col-6">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quidem ut id, necessitatibus, cupiditate
+        accusamus
+        repudiandae voluptatibus commodi ipsum iste in atque, ad deserunt? Suscipit officia laudantium facilis officiis
+        dolores.</p>
+    </div>
+
+    <div class="col-3">
+      @include('partials._search-bar')
+      @include('partials._follow-box')
+    </div>
+  </div>
 @endsection
