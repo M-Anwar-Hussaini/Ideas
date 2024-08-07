8f7680497b4649887f54f4d1cad7e3a4b5bdbf91
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-08 01:08:14 +0430

    Add Gates to the serviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Idea;
+use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +24,16 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+        Gate::define('admin', function (User $user): bool {
+            return $user->is_admin;
+        });
+
+        Gate::define('ideas.delete', function (User $user, Idea $idea) {
+            return ($idea->user_id === $user->id) || $user->is_admin;
+        });
+
+        Gate::define('ideas.edit', function (User $user, Idea $idea) {
+            return ($idea->user_id === $user->id) || $user->is_admin;
+        });
     }
 }
