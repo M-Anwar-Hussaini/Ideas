2e886fb434314ba3efb2ae0c2310a5f24b7b37a1
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-08 00:08:29 +0430

    Refactor the web.php to add the admin route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('home');
-Route::get('/admin', [AdminDashboardController::class, 'index'])->name('admin.dashboard')->middleware('auth');
+Route::get('/admin', [AdminDashboardController::class, 'index'])->name('admin.dashboard')->middleware(['auth', 'admin']);
 Route::get('/feed', FeedController::class)->name('feed')->middleware('auth');
 
 Route::get('/terms', function () {
