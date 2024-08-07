0ed8edaf4f6801d564d5089d54bf510d4602b4e3
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-08 01:07:38 +0430

    Use Gate in the admin controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        Gate::authorize('admin');
         return view('admin.dashboard');
     }
 }
