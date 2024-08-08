0aa8563c9469e12c7273c2d848e4b6392e8152a5
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-08 10:00:52 +0430

    Remove the code from the Gate in the AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,13 +27,5 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('admin', function (User $user): bool {
             return $user->is_admin;
         });
-
-        Gate::define('ideas.delete', function (User $user, Idea $idea) {
-            return ($idea->user_id === $user->id) || $user->is_admin;
-        });
-
-        Gate::define('ideas.edit', function (User $user, Idea $idea) {
-            return ($idea->user_id === $user->id) || $user->is_admin;
-        });
     }
 }
